731502adc96ed0d2d7a3b83ca8a644abe9739cb8
Logan Young <logany2013@gmail.com>
2018-10-11 14:04:58 -0500

    initial commit

---
+++ game.py
@@ -0,0 +1,17 @@
+from board import Board
+from player import Player
+
+class Game:
+    def __init__(self):
+        pass
+    
+    
+    
+    
+    
+    
+    
+    
+if __name__ == "__main__":
+    # test code
+    pass
---
+++ player.py
@@ -0,0 +1,19 @@
+
+
+class Player:
+    def __init__(self):
+        self.name = self.get_name
+        self.choice = self.get_choice
+    
+    def get_name(self):
+        pass
+    
+    def get_choice(self, Board):
+        pass
+    
+    
+    
+if __name__ == "__main__":
+    # test code
+    pass
+    

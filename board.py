7d01951dae2ffbcf52212b564bba90248d193fc8
mosgood549 <marley549@icloud.com>
2018-10-11 14:12:51 -0500

    added init

--- board.py
+++ board.py
@@ -1,7 +1,27 @@
 
 
 class Board:
-    pass
+    def __init__(self):
+        self.board = [[]]
+        self.width = width
+        self.height = height
+    
+    def place_piece(self, piece, column):
+        pass
+
+    def empty_board(self):
+        pass
+    
+    def check_win(self):
+        pass
+    
+    def is_full(self):
+        pass
+    
+    def display_board(self):
+        pass
+        
+    
 
 
 
@@ -13,3 +33,4 @@ class Board:
 if __name__ == "__main__":
     #test code
     pass
+    

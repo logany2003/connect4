491e61b9a32113d43ba5692d4958e6315fe14907
Logan Young <logany2013@gmail.com>
2018-10-19 13:35:26 -0500

    added docs

--- player.py
+++ player.py
@@ -5,12 +5,14 @@ class Player:
         self.piece = piece         #Assigns piece
         self.name = self.get_name() # assigns player name
     
-    def get_name(self):     #Gets player name
+    def get_name(self):
+        '''Gets player name'''
         print("what is your name? ")
         name = input("> ")
         return name
 
-    def get_choice(self, Board): #Gets player choice
+    def get_choice(self, Board):
+        '''Gets player choice'''
         print(f"{self.name} which column would you like to place your piece in?")
         choice = int(input("> "))
         return choice

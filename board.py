d223127b19a71c2dbf8ddfc6f754d4152ccca4a4
mosgood549 <marley549@icloud.com>
2018-10-19 13:15:20 -0500

    fixed vertical check and is_full

--- board.py
+++ board.py
@@ -29,29 +29,30 @@ class Board:
                         if self.board[i][j] == self.board[i][j+1] == self.board[i][j+2] == self.board[i][j+3]:
                             return True
                         
-        for i in range(self.height-3):      # Vertical Check
-            if self.board[i][j] != " ":
-                vcheck = [self.board[i][j] , self.board[i+1][j], self.board[i+2][j], self.board[i+3][j]]
-                for c in range(len(vcheck)):
-                    if self.board[i][j] == self.board[i+1][j] == self.board[i+2][j] ==  self.board[i+3][j]:
-                        return True
+        for i in range(self.height-3):
+            for j in range (self.width-3):
+                if self.board[i][j] != " ":
+                    vcheck = [self.board[i][j] , self.board[i+1][j], self.board[i+2][j], self.board[i+3][j]]
+                    for c in range(len(vcheck)):
+                        if self.board[i][j] == self.board[i+1][j] == self.board[i+2][j] ==  self.board[i+3][j]:
+                            return True
                     
-        for i in range(self.height-3):      # right Diag Check
+        for i in range(self.height-3):      # major Diag Check
             for j in range(self.width-3):
                 if self.board[i][j] != " ":
-                    rdcheck = [self.board[i][j] , self.board[i+1][j+1], self.board[i+2][j+2], self.board[i+3][j+3]]
-                    for c in range(len(rdcheck)):
+                    mdcheck = [self.board[i][j] , self.board[i+1][j+1], self.board[i+2][j+2], self.board[i+3][j+3]]
+                    for c in range(len(mdcheck)):
                         if self.board[i][j] == self.board[i+1][j+1] == self.board[i+2][j+2] ==  self.board[i+3][j+3]:
                             return True
         
-#        for i in range(self.height-3):      # left Diag Check
-#            for j in range(self.width-3):
-#                if self.board[i][j] != " ":
-#                    ldcheck = [self.board[i][j] , self.board[i+1][j-1], self.board[i+2][j-2], self.board[i+3][j-3]]
-#                    for c in range(len(ldcheck)):
-#                        if self.board[i][j] == self.board[i+1][j-1] == self.board[i+2][j-2] ==  self.board[i+3][j-3]:
-#                            return True
-#            
+        for i in range(self.height-3):      # minor Diag Check
+            for j in range(self.width-3):
+                if self.board[i][j] != " ":
+                    midcheck = [self.board[i][j] , self.board[i+1][j-1], self.board[i+2][j-2], self.board[i+3][j-3]]
+                    for c in range(len(midcheck)):
+                        if self.board[i][j] == self.board[i+1][j-1] == self.board[i+2][j-2] ==  self.board[i+3][j-3]:
+                            return True
+            
             
                     
         
@@ -91,17 +92,20 @@ def main():
     
      b = Board(7,6)
      b.add_piece(1, "4")
-     b.add_piece(1, "0")
-     b.add_piece(1, "0")
+     b.add_piece(1, "4")
+     b.add_piece(1, "4")
      b.add_piece(1, "4")
      b.add_piece(2, "4")
      b.add_piece(3, "0")
      b.add_piece(4, "0")
      b.add_piece(5, "0")
-     b.add_piece(6, "0")
+     b.add_piece(5, "0")
      b.add_piece(2, "4")
+     b.add_piece(4, "0")
+     b.add_piece(4, "0")
      b.add_piece(3, "0")
-     b.add_piece(3, "4")
+     b.add_piece(3, "0")
+     b.add_piece(4, "4")
      
   
   
